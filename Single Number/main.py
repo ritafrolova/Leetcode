5ddfc3d38579a75af8351b3431c1119b521bba13
ritafrolova <ritulya.frolova.2007@gmail.com>
2023-04-28 00:48:07 +0600

    not finished yet

---
+++ Single Number/main.py
@@ -0,0 +1,14 @@
+def singleNumber(nums):
+        my_dict={}
+        for elem in nums:
+            if elem not in my_dict:
+                  my_dict[elem]=1
+            else:
+                  my_dict[elem]+=1
+            for val in my_dict.values():
+              if val ==1:
+                   for v in :
+                        return k                       
+                  
+print(singleNumber([2,2,1]))
+print(singleNumber([4,1,2,1,2]))
