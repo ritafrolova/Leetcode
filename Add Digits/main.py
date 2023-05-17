7802a4bc81d9eec56e7504738c665ba7b7152fba
ritafrolova <ritulya.frolova.2007@gmail.com>
2023-05-17 20:56:11 +0600

    not finish

---
+++ Add Digits/main.py
@@ -0,0 +1,11 @@
+def addDigits(num):
+    while len(str(num))>9:
+        if num<10:
+            return num
+        sum=0
+        while num:
+            sum+=num%10
+            num=num//10
+        return sum
+print(addDigits(38))    
+
